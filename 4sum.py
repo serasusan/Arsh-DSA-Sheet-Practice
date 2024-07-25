00b149cb0de45ae4b8091e4ac8cc1a428a1b6c14
Sera Susan Koshy <108021864+serasusan@users.noreply.github.com>
2024-07-25 19:43:21 +0530

    Update 4sum.py

--- 4sum.py
+++ 4sum.py
@@ -50,35 +50,36 @@ target = 9
 print(four_sum(nums,target))
 
 # O P T I M A L A P P R O A C H
-def four_sum(a,target):
-    n = len(a)
-    result = []
-    a.sort()
+class Solution:
+    def fourSum(self, a: List[int], target: int) -> List[List[int]]:
+        n = len(a)
+        result = []
+        a.sort()
 
-    for i in range(n):
-        if i>0 and a[i]==a[i-1]:
-            continue
-        for j in range(i+1,n):
-            if j>i+1 and a[j]==a[j-1]:
+        for i in range(n):
+            if i>0 and a[i]==a[i-1]:
                 continue
-            k = j+1
-            l = n-1
-            while k<l:
-                sum = a[i]+a[j]+a[k]+a[l]
-                if sum<target:
-                    k+=1
-                elif sum>target:
-                    l-=1
-                else:
-                    temp = [a[i],a[j],a[k],a[l]]
-                    result.append(temp)
-                    k+=1
-                    j-=1
-                    while k<l and a[k]==a[k-1]:
+            for j in range(i+1,n):
+                if j>i+1 and a[j]==a[j-1]:
+                    continue
+                k = j+1
+                l = n-1
+                while k<l:
+                    sum = a[i]+a[j]+a[k]+a[l]
+                    if sum<target:
                         k+=1
-                    while k<l and a[l]==a[l-1]:
+                    elif sum>target:
                         l-=1
-    return result
+                    else:
+                        temp = [a[i],a[j],a[k],a[l]]
+                        result.append(temp)
+                        k+=1
+                        l-=1
+                        while k<l and a[k]==a[k-1]:
+                            k+=1
+                        while k<l and a[l]==a[l+1]:
+                            l-=1
+        return result
 
 
 
